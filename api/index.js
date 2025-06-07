ed0cc399e599d02ca137d374743cc1923521d976
MYNAMEINBEAN <bmb6@birmingham.k12.mi.us>
2025-06-07 13:11:35 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import { Hyperbeam } from 'hyperbeam';
 
+const API_KEY = "sk_live_zzGSMfCLIzyTY9LAIpBO9RpDUzh-iLMU-PuCccrHCs0";  // <-- your key here
+
 export default async function handler(req, res) {
   try {
-    const hb = await Hyperbeam(process.env.HYPERBEAM_API_KEY);
+    const hb = await Hyperbeam(API_KEY);
     const { url } = await hb.new();
     res.status(200).json({ url });
   } catch (err) {
